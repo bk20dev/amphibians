71b9a7046cbee6fbf89bf9291a1df255ecbf6116
Bartosz Bieniek <itsbk20@gmail.com>
2022-08-24 19:51:57 +0200

    Add status icon to the amphibian list layout

--- app/src/main/java/pl/bk20/amphibians/BindingAdapters.kt
+++ app/src/main/java/pl/bk20/amphibians/BindingAdapters.kt
@@ -1,8 +1,11 @@
 package pl.bk20.amphibians
 
+import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import pl.bk20.amphibians.adapter.AmphibianListAdapter
+import pl.bk20.amphibians.data.AmphibianApiStatus
 import pl.bk20.amphibians.network.Amphibian
 
 @BindingAdapter("listData")
@@ -10,5 +13,26 @@ fun listData(recyclerView: RecyclerView, list: List<Amphibian>?) {
     list?.let {
         val adapter = recyclerView.adapter as AmphibianListAdapter
         adapter.submitList(list)
+    }
+}
+
+@BindingAdapter("amphibianApiStatus")
+fun amphibianApiStatus(imageView: ImageView, status: AmphibianApiStatus?) {
+    status?.let {
+        when (status) {
+            AmphibianApiStatus.DONE -> {
+                imageView.visibility = View.GONE
+            }
+            AmphibianApiStatus.ERROR -> {
+                imageView.setImageResource(R.drawable.ic_offline)
+                imageView.contentDescription = "Unable to load the amphibian list"
+                imageView.visibility = View.VISIBLE
+            }
+            AmphibianApiStatus.LOADING -> {
+                imageView.setImageResource(R.drawable.ic_loading)
+                imageView.contentDescription = "Loading amphibians"
+                imageView.visibility = View.VISIBLE
+            }
+        }
     }
 }
