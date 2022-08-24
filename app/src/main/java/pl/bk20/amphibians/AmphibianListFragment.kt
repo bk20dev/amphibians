e4994ac6ccb46eedb44c956302e500293a78f530
Bartosz Bieniek <itsbk20@gmail.com>
2022-08-24 18:25:56 +0200

    Implement loading data from the internet Add missing permission to AndroidManifest Bind lifecycleOwner and viewModel to the AmphibianListFragment

--- app/src/main/java/pl/bk20/amphibians/AmphibianListFragment.kt
+++ app/src/main/java/pl/bk20/amphibians/AmphibianListFragment.kt
@@ -5,15 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import pl.bk20.amphibians.data.AmphibianViewModel
 import pl.bk20.amphibians.databinding.FragmentAmphibianListBinding
 
 class AmphibianListFragment : Fragment() {
+    private val viewModel: AmphibianViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentAmphibianListBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
         return binding.root
     }
 }
--- app/src/main/java/pl/bk20/amphibians/data/AmphibianViewModel.kt
+++ app/src/main/java/pl/bk20/amphibians/data/AmphibianViewModel.kt
@@ -3,8 +3,35 @@ package pl.bk20.amphibians.data
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import pl.bk20.amphibians.network.Amphibian
+import pl.bk20.amphibians.network.AmphibianApi
+
+enum class AmphibianApiStatus {
+    LOADING, DONE, ERROR
+}
 
 class AmphibianViewModel : ViewModel() {
-    private val _amphibianList = MutableLiveData<List<String>>()
-    val amphibianList: LiveData<List<String>> = _amphibianList
+    private val _amphibianList = MutableLiveData<List<Amphibian>>()
+    val amphibianList: LiveData<List<Amphibian>> = _amphibianList
+
+    private val _status = MutableLiveData(AmphibianApiStatus.LOADING)
+    val status: LiveData<AmphibianApiStatus> = _status
+
+    init {
+        loadAmphibians()
+    }
+
+    private fun loadAmphibians() {
+        viewModelScope.launch {
+            _status.value = AmphibianApiStatus.LOADING
+            try {
+                _amphibianList.value = AmphibianApi.retrofitService.getAmphibians()
+                _status.value = AmphibianApiStatus.DONE
+            } catch (exception: Exception) {
+                _status.value = AmphibianApiStatus.ERROR
+            }
+        }
+    }
 }
